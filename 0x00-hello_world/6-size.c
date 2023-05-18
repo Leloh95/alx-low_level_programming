971082b8aa7f13efde48383cb6eda07d81a651da
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-05-18 09:40:40 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,5 +10,5 @@ int main(void)
 	print("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	print("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	print("Size of a float: %lu byte(s)\n", sizeof(float));
-return 0;
+	return 0;
 }
