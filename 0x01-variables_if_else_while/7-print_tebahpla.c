1e0ec15fc4be0aff882ab0456a1eb24dad5de3cd
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-05-19 14:29:52 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - start program
+ * Return: 0(success)
+ */
+
+int main(void)
+{
+	int b;
+	b = 'z';
+	while (b >= 'a')
+	{
+		putchar(b);
+		b--;
+	}
+	putchar('\n');
+	return (0);
+}
