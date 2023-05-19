280832a4787d9f794fe37b85a1abd28e59610ae2
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-05-19 14:53:54 -0700

    commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - start program
+ * Return: 0(success)
+ */
+
+int main(void)
+{
+	char b;
+	int c;
+	b = 'a';
+	c = 0;
+
+	while (c < 10)
+	{
+		putchar(c + '0');
+		c++;
+	}
+	while (c <= 'f')
+	{
+		putchar(b);
+		b++;
+	}
+		putchar('\n');
+		return (0);
+}
