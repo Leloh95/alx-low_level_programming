bac84fbb6e895acaaabed4468b759815a23d97dd
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-05-19 08:51:16 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - start program
+ * Return: 0(success)
+ */
+int main(void)
+{
+int digit;
+
+for (digit = 0; digit < 10; digit++)
+putchar("%d", digit);
+putchar("\n");
+
+return (0);
+}
