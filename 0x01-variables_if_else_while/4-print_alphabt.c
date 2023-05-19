fb8f0422dbc04b5f858ceaf145b09d083b41c671
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-05-19 08:31:09 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - start of program
+ * Return : 0(success)
+*/
+ int main (void)
+{
+char alphaL;
+for (alphaL = 'a'; alphaL <= 'z'; alphaL++)
+{	
+if (alphaL != 'e' && alphaL != 'q')
+putchar (alphaL);
+}
+putchar ('\n');
+return (0);
+}
