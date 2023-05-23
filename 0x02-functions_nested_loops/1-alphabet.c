5bc8d01263aa89f7032dabe8139c75672277740e
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-05-23 11:31:19 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - start program
+ * Return: 0(success)
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		_putchar(alphabet);
+
+	_putchar('\n');
+		return (0);
+}
