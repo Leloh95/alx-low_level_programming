4df30c3932bdd1391e373f55954bf85ab9894415
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-05-24 07:50:48 -0700

    correction

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -4,11 +4,11 @@
  * _putchar - input the character c to stdout
  * @c: the character to print
  * @n: the input character
- * @a: first integer
- * @b: second integer
+ * @a: first input integer
+ * @b: second input integer
  * Return: (success)
  */
 int _putchar(char c)
 {
- return (write(1, &c, 1));
+ return (write(1, & c, 1));
 }
