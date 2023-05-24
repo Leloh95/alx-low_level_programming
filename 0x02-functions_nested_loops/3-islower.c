5e8098a5a601aecd9bead17b942b039abdcbee1c
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-05-24 00:33:57 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,26 @@
+#include"main.h"
+
+/**
+ * int _islower - print lowercase characters
+ * _putchar - print lowercase characters
+ * Return: 0
+ */
+
+int _islower(int c)
+{
+
+	char l = 'a';
+	if (l <= 'z')
+	{
+		_putchar(l);
+	}
+	_putchar('\n');
+	return (1);
+	if (l = 'A' && l <= 'Z')
+	return (0);
+}
+
+
+
+
+
