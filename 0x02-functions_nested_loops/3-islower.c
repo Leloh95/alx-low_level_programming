790b146870216dd136ab7a1b78e775495622cbac
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-05-24 07:43:53 -0700

    correction

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,11 +2,11 @@
 
 /**
 * _islower - print lowercase characters
-* @c - input for character
+*@c - input for character
 *main - start program
+*_putchar - print out character
 *Return: 0
 **/
-
 int _islower(int c)
 {
 if (c >= 97 && c <= 122)
