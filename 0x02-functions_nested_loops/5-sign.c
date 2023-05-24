77da4209326e4ca0f4bb8ce107183152143a7dfc
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-05-24 01:52:36 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,13 +8,19 @@
 
 int print_sign(int n)
 {
-	if (n > 0)
-		_putchar(43);
-		return (1);
-	if (n == 0)
-		_putchar(48);
-		return (0);
-	if else (n <0)
-		_putchar(45);
-		return (-1);
+if (n > 0)
+{
+	_putchar(43);
+	return (1);
+}
+if (n == 0)
+{
+	_putchar(48);
+	return (0);
+}
+if else (n <0)
+{
+	_putchar(45);
+	return (-1);
+}
 }
