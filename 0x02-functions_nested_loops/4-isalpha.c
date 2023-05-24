a3e11116d45f8324bd5cd8d9843aee369e6aec9c
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-05-24 01:02:16 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,17 @@
 #include"main.h"
 
 /**
- * int _islower - print lowercase characters
- * c - characters
+ * int _isalpha - print lowercase characters
+ * c - input characters
  * main - start program
  * Return: 0
  */
 
-int _islower(int c)
+int _isalpha(int c)
 {
 
-if (c >= 65 && c <= 122)
+if (c >= 97 && c <= 122)
         return (1);
-return (0);
+if (c>= 65 && c<= 90)
+	return (0);
 }
