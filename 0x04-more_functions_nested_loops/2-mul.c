ac1f5f0c40a4ba5b406081cb6e1470471183195d
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-05-25 07:55:55 -0700

    2mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+*mul - multipiew integers
+*@a: integer first
+*@b: integer second
+*@p: product of multiplication
+*_putchar - prints put characters
+*Return: 0(success)
+*/
+
+int mul(int a, int b)
+{
+	p = a * b;
+
+	_putchar('c');
+}
