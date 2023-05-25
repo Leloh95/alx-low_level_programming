9f38492b7cc1a6fa6cc212611a62991f7656d455
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-05-25 07:13:58 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+* _putchar - print out characters
+*_isupper - function to print characters
+* @c: character
+* Return: 0(success)
+ */
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}
