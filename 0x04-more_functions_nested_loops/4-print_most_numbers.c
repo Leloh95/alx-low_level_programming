076b9a2aac695780e1b24285a4a03299c14cd526
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-05-25 09:14:04 -0700

    printmostnumbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,23 +1,23 @@
 #include"main.h"
 /**
- * print_numbers - check numbers from 0 to 9
- *_putchar - prints output
- * Return: 0(success)
- */
+* print_numbers - check numbers from 0 to 9
+*putchar - prints output
+* Return: 0(success)
+*/
 void print_numbers(void)
 {
-	int b = 0;
+int b = 0;
 
-	for (; b <= 9; b++)
-	{
-		if  (b == 2 || b == 4)
-		{
-		continue;
-		}
-		else
-		{
-		_putchar(b + '0');
-		}
-	}
-	_putchar('\n');
+for (; b <= 9; b++)
+{
+if  (b == 2 || b == 4)
+{
+continue;
+}
+else
+{
+putchar(b + '0');
+}
+}
+putchar('\n');
 }
