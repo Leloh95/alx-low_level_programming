9736f1e9d1369f7af371fb1f7328bcdc6036a554
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-05-25 07:19:25 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,11 @@
 #include"main.h"
+#include "_putchar.c"
 /**
-* _putchar - print out characters
+*_putchar - print out characters
 *_isupper - function to print characters
-* @c: character
-* Return: 0(success)
- */
+*@c: character
+*Return: 0(success)
+*/
 int _isupper(int c)
 {
 if (c >= 65 && c <= 90)
---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,4 @@
+_putchar - print out characters
+_isupper - function to print characters
+@c: character
+Return: 0(success)
