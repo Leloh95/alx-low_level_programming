7215b30ff66417818643590749c4b33a6f9ec488
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-05-25 10:11:18 -0700

    more lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+* print_line - prints out linea
+* putchar - print out lines
+*@n: input character
+*/
+
+void print_line(int n)
+{
+n = 0;
+;
+if (n <= 6)
+{
+putchar('-');
+}
+else
+{
+putchar('\n');
+}
+putchar('\n');
+}
