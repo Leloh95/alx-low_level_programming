fa5ff2ac833663f3096636e4259e60739f08d1b8
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-05-25 12:36:58 -0700

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -3,7 +3,7 @@
 *print_square - print out size
 *@n - count
 *@size: size of square
-*_putchar - print out characters
+*putchar - print out characters
 */
 
 void print_square(int size)
@@ -12,7 +12,7 @@ int a, b;
 
 if (size <= 0)
 {
-_putchar('\n');
+putchar('\n');
 }
 else
 {
@@ -20,9 +20,9 @@ for (b = 0; b < size; b++)
 {
 for (a = 0; a < size; a++)
 {
-_putchar(65);
+putchar(65);
 }
-_putchar('\n');
+putchar('\n');
 }
 }
 }
