9a2db2968b7b6611ee8f2d2cd2023156a5112105
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-05-25 08:17:03 -0700

    3printnumbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * print_numbers - check numbers from 0 to 9
+ * Return: 0(success)
+ */
+
+void print_numbers(void)
+{
+int b;
+
+for (b = 0; b < 9; b++)
+{
+_putchar(b);
+}
+_putchar('\n');
+}
+
