5b65f3ea09f17be0e203392adf08725c4cf0a6b3
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-05-29 03:12:06 -0700

    0Reset98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes integer to pointer and update value
+ * @n: integer value
+ */
+
+void reset_to_98(int *n)
+{
+int *n;
+n = 98;
+
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - input the character c to stdout
+ * @c: the character to print
+ * @n: the input character
+ * @a: first input integer
+ * @b: second input integer
+ * Return: (success)
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1)
