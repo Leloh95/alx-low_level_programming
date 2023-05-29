8e8dd6b5dbbfba5c89e051d26ff8c5be372c638b
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-05-29 07:22:21 -0700

    5rev

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,7 +2,7 @@
 
 /**
  *rev_string - print reverse string
- *@s: string 
+ *@s: string
  */
 
 void rev_string(char *s)
@@ -14,8 +14,8 @@ while (s[index++])
 len++;
 for (index = len - 1; index >= len / 2; index--)
 {
-	rev = s[index];
-	s[index] = s[len - index - 1];
-	s[len - index - 1] = rev;
+rev = s[index];
+s[index] = s[len - index - 1];
+s[len - index - 1] = rev;
 }
 }
