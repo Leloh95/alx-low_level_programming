c87ae700680ba808cd4ea689e9e10fd199a92f7f
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-06-01 08:38:29 -0700

    6-cap

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include"main.h"
+
+/**
+ *cap_string - capitalizes all words of string
+ *@s: string
+ *Return: returns string
+ */
+
+char *cap_string(char *s)
+{
+int index = 0;
+
+while (s[index])
+{
+while (!(s[index] >= 'a' && s[index] <= 'z'))
+index++;
+if (s[index - 1] == ' ' ||
+s[index - 1] == '\t' ||
+s[index - 1] == '\n' ||
+s[index - 1] == ',' ||
+s[index - 1] == ';' ||
+s[index - 1] == '.' ||
+s[index - 1] == '!' ||
+s[index - 1] == '?' ||
+s[index - 1] == '"' ||
+s[index - 1] == '(' ||
+s[index - 1] == ')' ||
+s[index - 1] == '{' ||
+s[index - 1] == '}' ||
+index == 0)
+s[index] -= 32;
+index++;
+}
