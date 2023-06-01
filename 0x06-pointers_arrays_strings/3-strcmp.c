286fcfbead344bacce670656be0b179f631cb62d
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-06-01 09:34:57 -0700

    3strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,7 @@
  * _strcmp - compares two strings
  * @s1: first string
  * @s2: second string
- * Return: dest
+ * Return: s1[a] - s2[a]
  */
 
 int _strcmp(char *s1, char *s2)
@@ -16,7 +16,7 @@ while (s1[a] != '\0' && s2[a] != '\0')
 {
 if (s1[a] != s2[a])
 {
-return(s1[a] - s2[a]);
+return (s1[a] - s2[a]);
 }
 a++;
 }
