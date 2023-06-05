dec6eb519a88394cd32aa6b7bbd9382c636318af
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-06-05 11:41:42 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,17 +13,17 @@ int i, valu = 0;
 
 while (*s)
 {
-	for (i = 0; accept[i]; i++)
-	{
-		if (accept[i] == *s)
-		{
-			valu++;
-			break;
-		}
-		else if ((accept[i + 1]) == "\0")
-			return (valu);
-	}
-	s++;
+for (i = 0; accept[i]; i++)
+{
+if (accept[i] == *s)
+{
+valu++;
+break;
+}
+else if ((accept[i + 1]) == '\0')
+return (valu);
+}
+s++;
 }
 return (valu);
 }
