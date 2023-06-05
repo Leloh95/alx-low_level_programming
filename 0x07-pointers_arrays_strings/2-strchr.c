8e8f24b925843aeade555acfa9a4e9664aca8cb9
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-06-05 06:02:46 -0700

    2-strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * _strchr - locates character in a string
+ * @s: character
+ * @c: character
+ * Return: 0(success)
+ */
+
+char *_strchr(char *s, char c)
+{
+	int a;
+for (a = 0; s[a] >= '\0'; a++)
+{
+	if (s[a] == c)
+		return (s + a);
+}
+return (NULL);
+}
