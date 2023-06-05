f0593d581996d8df6ad2678f8c14242acea4bf2b
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-06-05 06:41:33 -0700

    3-strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include"main.h"
+
+/**
+ * _strspn - gets string length
+ * @s:string
+ * @accept:characters
+ * Return: 0(success)
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int a, b, integer, check;
+
+integer = 0;
+
+for (a = 0; s[a] != '\0'; a++)
+{
+check = 0;
+for (b = 0; accept[b] != '\0'; b++)
+{
+if (accept[b] == s[a])
+{
+integer++;
+check = 1;
+}
+}
+}
+}
