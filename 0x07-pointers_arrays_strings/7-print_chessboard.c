77bfc7c3ef00edec20537ddb47c0c3f59f0e25ad
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-06-05 12:05:22 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -11,11 +11,10 @@ void print_chessboard(char (*a)[8])
 {
 int c, b;
 
-for (c = 0; c < 8; c++)
+for (c = 0; a[c][7]; c++)
 {
 for (b = 0; b < 8; b++)
 _putchar(a[c][b]);
-}
 _putchar('\n');
-
+}
 }
