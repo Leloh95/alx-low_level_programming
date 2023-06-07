072f63c92acb7e3813e6c0b9b5a7d1361d101554
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-06-07 06:27:36 -0700

    5-sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_sqrt - print out squares
+ * @n: input
+ * Return: 0(success)
+ */
+
+int _sqrt_recursion(int n)
+{
+	int sqrt = i * i;
+
+	if (sqrt > n)
+return (-1);
+
+	if (sqrt == n)
+return (i);
+	
+return (_sqrt(n, i + 1));
+}
+
