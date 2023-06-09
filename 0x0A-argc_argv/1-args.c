ad39573e7a1dd1afe92e6873a5991d06f12f2cd8
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-06-09 04:47:05 -0700

    1-args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+ * main - start of program
+ * @argc: counts arguments passed to it
+ * @argv: counts strings
+ * Return: 0(success)
+ */
+
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", argc - 1);
+return (0);
+}
