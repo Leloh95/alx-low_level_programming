bf011c62540fda29b6a22d885250e891c65aedbc
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-06-09 06:57:51 -0700

    4-add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,6 @@
 #include"main.h"
 #include<stdio.h>
-#include<stdlib>
+#include<stdlib.h>
 #include<string.h>
 
 /**
@@ -10,7 +10,7 @@
  * Return: 0(success)
  */
 
-int main(int argc, int *argv[])
+int main(int argc, char *argv[])
 {
 	int a;
 	unsigned int b, sum = 0;
@@ -21,7 +21,7 @@ int main(int argc, int *argv[])
 		for (a = 1; a < argc; a++)
 		{
 			c = argv[a];
-			for (b = 0; v < strlen(c); b++)
+			for (b = 0; b < strlen(c); b++)
 			{
 				if (c[b] < 48 || c[b] > 57)
 				{
