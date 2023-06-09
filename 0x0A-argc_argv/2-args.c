e5435efbc9d35069992b1e7f6703b5d200bb9194
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-06-09 05:06:34 -0700

    2-args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+ * main - start of program
+ * @argc: counts arguments passed to it
+ * @argv: counts strings
+ * @i: counter
+ * Return: 0(success)
+ */
+
+int main(int argc, char *argv[])
+{
+int i = 0;
+
+if (argc > i)
+printf("%s\n", argv[i]);
+i++;
+return (argv[i]);
+}
