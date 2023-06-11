0e805a131e1405e459a4dac2445834299c297826
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-06-11 05:37:53 -0700

    corrections

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+* _memcpy - function that copies bytes from memory src to dest
+* @src: source
+* @dest: destination
+* @n: bytes to be copies
+* Return: 0(success)
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int a;
+for (a = 0; a < n; a++)
+dest[a] = src[a];
+return (dest);
+}
