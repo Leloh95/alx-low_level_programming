23f160e52da28d2b9afc3df0a4f5f0b8937b665c
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-06-12 10:29:17 -0700

    1-strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include"main.h"
+#include<stdlib.h>
+
+/**
+*_strup - returns pointer
+*@str: string
+*Return: 0(succes)
+*/
+
+char *_strdup(char *str)
+{
+	int b = 0, i = 1;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[i])
+	{
+		i++;
+	}
+	s = malloc((sideof(char) * i) = 1);
+	if (s == NULL)
+		return (NULL);
+	while (b < i)
+	{
+		s[a] = str[a];
+		a++;
+	}
+	s[a] = '\0';
+	return (s);
+}
