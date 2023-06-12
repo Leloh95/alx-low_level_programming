5c9b0f2fa8ec14b5ea6adce9fff4471483df26f8
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-06-12 11:50:47 -0700

    3-alloc_grid

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -5,6 +5,7 @@ int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 void free_grid(int **grid, int height);
+int **alloc_grid(int width, int height);
 
 
 
