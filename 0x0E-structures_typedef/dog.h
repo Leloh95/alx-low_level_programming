86af54d2937a3748fc9cb8e22b0fc61e1d14c16a
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-06-19 08:27:51 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,10 @@
+#include<stdlib.h>
+
+struct dog
+{
+char *name;
+char *owner;
+float age;
+};
+
+
---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H 
+#define MAIN_H
+#define ABS(x)
+int _putchar(char c);
+int main(void);
+#endif /* MAIN_H */
+~
