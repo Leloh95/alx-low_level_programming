36237a451291bd1de12cd4976037d8a3a6b99c8f
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-06-19 14:39:04 -0700

    4-new

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,33 +11,4 @@
 *Return: 0(success)
 */
 
-dog_t *new_dog(char *name, float age, char *owner)
-{
-	dog_t = NULL;
-
-dog_t *dog = malloc(sizeof(dog_t));
-
-	if (dog == NULL)
-{
-	return (NULL);
-}
-dog->name = malloc(strlen(name) + 1);
-	if (dog->name == NULL)
-{
-	free(dog);
-	return (NULL);
-}
-strcpy(dog->name, name);
-dog->owner = malloc(strlen(owner) + 1);
-
-	if (dog->owner == NULL)
-{
-	free(dog->name);
-	free(dog);
-	return (NULL);
-}
-strcpy(dog->owner, owner);
-dog->age = age;
-return (dog);
-}
-
+dog_t *new_dog(char *name,
