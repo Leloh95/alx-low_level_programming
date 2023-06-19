00052dd5131c0d4360652fc2657398b3747f23e1
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-06-19 11:35:41 -0700

    corrections

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
-#ifndef DOG_H
-#define DOG_H
-#include<stdlib.h>
+#ifndef _HEADER_
+#define _HEADER_
 
 /**
  * struct dog - changes struct
@@ -8,21 +7,20 @@
  *@owner: user
  *@age: user age
  */
-
 struct dog
 {
 char *name;
-char *owner;
 float age;
+char *owner;
 };
 
 /** 
  * dog_t - typedef for dog structure
  */
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
 #endif
