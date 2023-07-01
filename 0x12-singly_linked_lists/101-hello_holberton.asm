fc1841e82b7bdbac0d400676b19d1f7100fc8c95
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-07-01 12:38:52 -0700

    holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,10 +1,14 @@
-global   start
-	  extern    printf
-start:
-	  mov   edi, format
-	  xor   eax, eax
-	  call  printf
-	  mov   eax, 60
-	  ret
-format: db `Hello, Holberton\n`,0
+SECTION .text
+	global main
+	extern printf
+main:
+	mov esi, msg
+	mov edi, format
+	mov eax, 0
+	call printf
 
+	mov eax, 0
+	ret
+SECTION .data
+msg: db "Hello, Holberton", 0
+format: db "%s", 10, 0
