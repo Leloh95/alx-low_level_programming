8c2e8e32c278535d5c3c44448a710f64cd5ec0e2
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-07-04 06:40:30 -0700

    1-listint

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,8 +1,19 @@
 #include<stdio.h>
-#include"lists.h"4
+#include"lists.h"
 
 /**
  * listint_len - prints number of elements
  * @h: pointer
  * Return: number of elements
+ */
 size_t listint_len(const listint_t *h)
+{
+	size_t elem = 0;
+
+	while (h != NULL)
+	{
+		elem++;
+		h = h->next;
+	}
+return (element);
+}
