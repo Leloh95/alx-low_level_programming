045fa25da6e131175edfc60d9644e79120292f94
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-07-04 11:23:03 -0700

    5-free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include"lists.h"
+
+/**
+ * free_listint2 - free listint
+ * @head: first node
+ *Return: 0
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *headp;
+
+if (head == NULL)
+return;
+
+while (*head)
+{
+	headp = (*head)->next;
+	free(*head);
+	*head = headp;
+}
+*head = NULL;
+}
