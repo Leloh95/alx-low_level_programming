9d91e77f5f582c283da1b8d2ca25580bc1974e6c
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-07-06 08:08:31 -0700

    corrections

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -11,42 +11,42 @@ size_t print_listint_safe(const listint_t *head);
  */
 size_t looped_listint_len(const listint_t *head)
 {
-	const listint_t *big, *same;
-	size_t node = 1;
+const listint_t *big, *same;
+size_t node = 1;
 
-	if (head == NULL || head->next == NULL)
-		return (0);
+if (head == NULL || head->next == NULL)
+return (0);
 
-	big = head->next;
-	same = (head->next)->next;
+big = head->next;
+same = (head->next)->next;
 
-	while (same)
-	{
-		if (big == same)
-
-			big = head;
-			while (big != same)
-			{
-				node++;
-				big = big->next;
-				same = same->next;
-			}
-
-			big = big->next;
-			while (big != same)
-			{
-				node++;
-				big = big->next;
-			}
-			return (node);
-		}
-
-		big = big->next;
-		same = (same->next)->next;
-	}
-
-	return (0);
+while (same)
+{
+if (big == same)
+{
+big = head;
+while (big != same)
+{
+node++;
+big = big->next;
+same = same->next;
 }
+
+big = big->next;
+while (big != same)
+{
+node++;
+big = big->next;
+}
+return (node);
+}
+
+big = big->next;
+same = (same->next)->next;
+}
+return (0);
+}
+
 
 /**
  * print_listint_safe - Prints a listint_t list
@@ -55,31 +55,28 @@ size_t looped_listint_len(const listint_t *head)
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t node, idx = 0;
+size_t node, idx = 0;
 
-	node = looped_listint_len(head);
+node = looped_listint_len(head);
 
-	if (node == 0)
-	{
-		for (; head != NULL; node++)
-		{
-			printf("[%p] %d\n", (void *)head, head->n);
-			head = head->next;
-		}
-	}
-
-	else
-	{
-		for (idx = 0; idx < node; idx++)
-		{
-			printf("[%p] %d\n", (void *)head, head->n);
-			head = head->next;
-		}
-
-		printf("-> [%p] %d\n", (void *)head, head->n);
-	}
-
-	return (node);
+if (node == 0)
+{
+for (; head != NULL; node++)
+{
+printf("[%p] %d\n", (void *)head, head->n);
+head = head->next;
+}
+}
+else
+{
+for (idx = 0; idx < node; idx++)
+{
+printf("[%p] %d\n", (void *)head, head->n);
+head = head->next;
 }
 
+printf("-> [%p] %d\n", (void *)head, head->n);
+}
 
+return (node);
+}
