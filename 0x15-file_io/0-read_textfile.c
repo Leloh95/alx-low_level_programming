f06534421c9c0f7a7be52ef9203448cb68fad4fd
Leloh95 <nompumeleloshabalala8@gmail.com>
2023-07-11 03:13:20 -0700

    0-read

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile- Read text file print to the posix.
+ * @filename: file to be read
+ * @letters: letters to be read
+ * Return: 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *lett;
+	ssize_t readit;
+	ssize_t fileit;
+	ssize_t wryt;
+
+fileit = open(filename, O_RDONLY);
+if (try == -1)
+return (0);
+
+lett = malloc(sizeof(char) * letters);
+readit = read(fileit, lett, letters);
+wryt = write(STDOUT_FILENO, lett, readit);
+free(lett);
+close(fileit);
+return (wryt);
+}
+
